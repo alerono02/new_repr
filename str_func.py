2df38b222ea3ad9a1759543ea8e85531016570a8
Aleksandr <ribakov.alex2013@yandex.ru>
2023-07-31 18:47:19 +0300

    append func title

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def str_func(string: str):
     """Get string and return upper"""
     return string.upper()
+
+
+def title_letter(string: str):
+    """Make 1st letter upper"""
+    return string.title()
